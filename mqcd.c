f8186f66c6ea9f6fc71518e939ac42095dfedfcf
Meqr1 <ayanmahajan41@gmail.com>
2024-10-09 16:40:42 +0530

    added history reading

--- mqcd.c
+++ mqcd.c
@@ -10,21 +10,48 @@ int writeHistory(char *directory) {
   }
 
   char filePath[1024];
-    snprintf(filePath, sizeof(filePath), "%s\\.history", homeDir);
+  snprintf(filePath, sizeof(filePath), "%s\\.history", homeDir);
 
-    FILE *file = fopen(filePath, "w");
-    if (file == NULL) {
-        printf("Error: Could not open file %s for writing.\n", filePath);
-        return 1;
-    }
+  FILE *file = fopen(filePath, "w");
+  if (file == NULL) {
+    printf("Error: Could not open file %s for writing.\n", filePath);
+    return 1;
+  }
 
-    fprintf(file, "%s", directory);
+  fprintf(file, "%s", directory);
 
-    fclose(file);
+  fclose(file);
 
-    printf("Data written to %s successfully.\n", filePath);
+  printf("Data written to %s successfully.\n", filePath);
 
-    return 0;
+  return 0;
+}
+
+int readhistory() {
+  const char *homeDir = getenv("USERPROFILE");
+
+  if (homeDir == NULL) {
+    printf("Error: Could not determine the home directory.\n");
+    return 1;
+  }
+
+  char filepath[1024];
+  snprintf(filepath, sizeof(filepath), "%s\\.history", homeDir);
+
+  FILE *file = fopen(filepath, "r");
+  if (file == NULL) 
+  {
+      printf("Error: Could not open file %s for reading. \n", filepath);
+      return 1;
+  }
+
+  char buffer[256];
+
+  while (fgets(buffer, sizeof(buffer), file) != NULL) {
+      printf("%s", buffer);
+  };
+
+  return 0;
 }
 
 int main(int argc, char **argv) {
@@ -36,6 +63,7 @@ int main(int argc, char **argv) {
   if (strcmp("set", argv[1]) == 0) {
     writeHistory(argv[2]);
   } else if (strcmp("get", argv[1]) == 0) {
+    readhistory();
   }
 
   return 0;
